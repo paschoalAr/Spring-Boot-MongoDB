b390b6531d9b290468a34a3da4d36782474b0744
paschoalAr <arthur.paschoal@edu.pucrs.br>
2024-08-29 08:52:40 -0300

    Criação do projeto

---
+++ src/main/java/com/paschoal/api_restful_sring_mongodb/ApiRestfulSringMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.paschoal.api_restful_sring_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulSringMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulSringMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paschoal/api_restful_sring_mongodb/ApiRestfulSringMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paschoal.api_restful_sring_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulSringMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
